064efccd87e0d84ac974999cf5d674684c03859a
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-11 19:26:52 -0700

    adding schema types to typeDefs

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,29 @@
+const typeDefs = `
+    type User {
+        _id: ID
+        username: String,
+        posts: [Post]
+        friends: [ID]
+    }
+    type Post {
+        _id: ID
+        text: String
+        body: String
+        userId: String
+        comments: [Comment]
+        category: String
+    }
+    type Comment {
+        _id: ID
+        postId: String
+        body: String
+        userId: String
+        likes: Int
+    }
+    type Auth {
+        token: ID!
+        user: User
+    }
+`
+
+module.exports = typeDefs;
