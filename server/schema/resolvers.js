f530b90e6495c7765b0b7bf5eef4f457778a09d8
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-11 19:01:05 -0700

    adding login mutation and 'me' query into resolvers

---
+++ server/schema/resolvers.js
@@ -0,0 +1,30 @@
+const { signToken } = require('../utils/auth');
+const { User } = require('../models');
+
+const resolvers = {
+    Query: {
+        me: async (parent, { username }) => {
+            return await User.findOne( { username: username })
+            .select('-__v -password');
+        },
+    },
+    Mutation: {
+        login: async (parent, { username, password }) => {
+            const user = await User.findOne({ username });
+            if (!user) {
+                throw new AuthenticationError('No user found with this username');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect password');
+            }
+            const token = signToken(user);
+            if (!token) {
+                throw new Error('Error signing token');
+            }
+            return { token, user };
+        }
+    }
+};
+
+module.exports = resolvers;
