5c1cdf64d19a326c0847a983caaa8a9539ee2c6e
Shelby Hutchinson <shhutc18@gmail.com>
2024-04-16 09:22:17 -0700

    navbar ui

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,35 +1,48 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link as RouterLink } from 'react-router-dom';
+import { AppBar, Toolbar, Typography, Button, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+    color: 'yellow', // change color
+    fontFamily: 'Courier New', // change font
+  },
+  link: {
+    color: 'inherit',
+    textDecoration: 'none',
+  },
+  specialLink: {
+    color: 'yellow', // change color
+    textDecoration: 'none',
+    fontFamily: 'Courier New', // change font
+  },
+}));
 
 const Navbar = () => {
-  const navbarStyle = {
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
-  position: 'fixed',
-  top: '10px',
-  width: 'fit-content',
-  margin: '0 auto',
-  left: 0,
-  right: 0,
-  marginBottom: 0,
-  zIndex: 1000, // Add this line
-};
-
-  const linkStyle = {
-    textDecoration: 'none',
-    color: 'black',
-    padding: '0 30px',
-    fontSize: '20px',
-    fontFamily: "'Work Sans', sans-serif",
-  };
+  const classes = useStyles();
 
   return (
-    <div style={navbarStyle}>
-      <Link style={linkStyle} to="/Home">Home</Link> {/* Change to Link */}
-      <Link style={linkStyle} to="/Login">Sign In</Link> {/* Change to Link */}
-      <Link style={linkStyle} to="/Register">Register</Link> {/* Change to Link */}
-      <Link style={linkStyle} to="/MyProfile">My Profile</Link> {/* Change to Link */}
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            <RouterLink to="/Home" className={classes.specialLink}>The Tech Umbrella</RouterLink>
+          </Typography>
+          <Button color="inherit">
+            <RouterLink to="/Login" className={classes.link}>Sign In</RouterLink>
+          </Button>
+          <Button color="inherit">
+            <RouterLink to="/Register" className={classes.link}>Register</RouterLink>
+          </Button>
+          <Button color="inherit">
+            <RouterLink to="/MyProfile" className={classes.link}>My Profile</RouterLink>
+          </Button>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
