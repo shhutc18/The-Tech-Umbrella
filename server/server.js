3158183a6db94f5d811899043d6be68106aa28d0
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-15 18:57:11 -0700

    fixed typo in the server.js file console logging undefined

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ const startAppolloServer = async () => {
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`🌍 Now listening on localhost:${PORT}`);
-      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
     });
   });
   
