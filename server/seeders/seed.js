7ea3bd4d003b1edaad9ebebd3bc5b3305bd75d51
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-18 13:37:32 -0700

    updating seed script

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -12,17 +12,6 @@ db.once('open', async () => {
 
     await User.create(userSeeds);
 
-    for (let i = 0; i < postSeeds.length; i++) {
-      const { _id, postAuthor } = await Post.create(postSeeds[i]);
-      const user = await User.findOneAndUpdate(
-        { username: postAuthor },
-        {
-          $addToSet: {
-            posts: _id,
-          },
-        }
-      );
-    }
   } catch (err) {
     console.error(err);
     process.exit(1);
