1701be6708dfa2b0a1acb3949c123e1f301e5403
Adam McMains <adammcmains@gmail.com>
2024-04-17 20:46:22 -0700

    add queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -19,3 +19,179 @@ export const post = gql`
         }
     }
 `;
+export const users = gql`
+query Users {
+users {
+    _id
+    username
+    posts {
+      _id
+      userId
+      title
+      body
+      category
+      comments {
+        _id
+        postId
+        body
+        userId
+        likes
+      }
+      likes
+    }
+    comments {
+      _id
+      postId
+      body
+      userId
+      likes
+    }
+    friends
+  }
+}
+`;
+
+export const User = gql`
+query User($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      posts {
+        likes
+        _id
+        userId
+        title
+        body
+        category
+        comments {
+          _id
+          postId
+          body
+          userId
+          likes
+        }
+      }
+      comments {
+        _id
+        postId
+        body
+        userId
+        likes
+      }
+      friends
+    }
+  }
+`;
+
+export const Category = gql`
+query Category($category: String!) {
+    category(category: $category) {
+      _id
+      userId
+      title
+      body
+      category
+      comments {
+        _id
+        postId
+        body
+        userId
+        likes
+      }
+      likes
+    }
+  }
+`;
+
+export const MostLikedPosts = gql`
+query MostLikedPosts {
+    mostLikedPosts {
+      _id
+      userId
+      title
+      body
+      category
+      comments {
+        _id
+        postId
+        body
+        userId
+        likes
+      }
+      likes
+    }
+  }
+`;
+
+export const userPosts = gql`
+query UserPosts($userId: ID!) {
+    userPosts(userId: $userId) {
+      _id
+      userId
+      title
+      body
+      category
+      comments {
+        _id
+        postId
+        body
+        userId
+        likes
+      }
+      likes
+    }
+  }
+`;
+
+export const UserComments = gql`
+query UserComments($userId: ID!) {
+    userComments(userId: $userId) {
+      _id
+      postId
+      likes
+      userId
+      body
+    }
+  }
+`;
+
+export const SubscribedPosts = gql`
+query SubscribedPosts($userId: ID!) {
+    subscribedPosts(userId: $userId) {
+      _id
+      userId
+      title
+      body
+      category
+      comments {
+        _id
+        body
+        postId
+        userId
+        likes
+      }
+      likes
+    }
+  }
+`;
+
+export const AnonymousBrowse = gql`
+query AnonymousBrowse {
+    anonymousBrowse {
+      _id
+      userId
+      title
+      body
+      category
+      comments {
+        _id
+        postId
+        body
+        userId
+        likes
+      }
+      likes
+    }
+  }
+`;
+
