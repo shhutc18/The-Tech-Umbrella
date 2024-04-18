1bc1c7655aa25db6bbda73d912cc3393abcad5ab
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-17 20:56:13 -0700

    installing bcrypt and hashing passwords on user create

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Post = require('./Post')
 const Comment = require('./Comment')
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -25,6 +26,21 @@ const userSchema = new mongoose.Schema({
         type: [Comment.schema]
     },
 });
+
+// hash user password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+  
+  // custom method to compare and validate password for logging in
+  userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+  };
 
 const User = mongoose.model('User', userSchema);
 
