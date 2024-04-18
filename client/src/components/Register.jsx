1171f4bdeb1415a115f8d2f3a9728b0e19a43c6c
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-17 19:40:47 -0700

    refactoring register form to create user with apollo server gql

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,6 +1,11 @@
 import { useState } from 'react';
 import { Button, TextField, Paper, Typography, makeStyles } from '@material-ui/core';
 
+import Auth from '../utils/auth';
+
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
@@ -21,13 +26,54 @@ const useStyles = makeStyles((theme) => ({
 
 const Register = () => {
   const classes = useStyles();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  const [showAlert, setShowAlert] = useState(false);
+  const [addUser] = useMutation(ADD_USER);
 
-  const handleRegister = () => {
-    // Perform registration logic here
-    console.log('Registering user:', username);
-    console.log('Password:', password);
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    // check if form has everything (as per react-bootstrap docs)
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    // Check if userFormData contains username, email, and password
+    if (!userFormData.username || !userFormData.email || !userFormData.password) {
+      console.error('Username, email, and password are required');
+      setShowAlert(true);
+      return;
+    }
+
+    try {
+      const response = await addUser({
+        variables: {
+          username: userFormData.username.toString(),
+          email: userFormData.email.toString(),
+          password: userFormData.password.toString(),
+        },
+      });
+
+      console.log(response);
+
+      Auth.login(response.data.addUser.token);
+    } catch (err) {
+      console.error(err);
+      setShowAlert(true);
+    }
+
+    setUserFormData({
+      username: '',
+      email: '',
+      password: '',
+    });
   };
 
   return (
@@ -38,7 +84,23 @@ const Register = () => {
       <Typography variant="body1" gutterBottom>
         Ready to start posting? Please fill out the form below to create a new account.
       </Typography>
-      <form className={classes.form} noValidate>
+      <form className={classes.form} noValidate onSubmit={handleFormSubmit} >
+        <Typography variant="body1" gutterBottom>
+          {showAlert ? '🚨 Something went wrong with your registration!' : ''}
+        </Typography>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email"
+          name="email"
+          autoComplete="email"
+          autoFocus
+          value={userFormData.email}
+          onChange={handleInputChange}
+        />
         <TextField
           variant="outlined"
           margin="normal"
@@ -49,8 +111,8 @@ const Register = () => {
           name="username"
           autoComplete="username"
           autoFocus
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
+          value={userFormData.username}
+          onChange={handleInputChange}
         />
         <TextField
           variant="outlined"
@@ -62,16 +124,16 @@ const Register = () => {
           type="password"
           id="password"
           autoComplete="current-password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          value={userFormData.password}
+          onChange={handleInputChange}
         />
         <Button
           type="submit"
           fullWidth
           variant="contained"
           color="primary"
+          disabled={!(userFormData.username && userFormData.email && userFormData.password)}
           className={classes.submit}
-          onClick={handleRegister}
         >
           Register
         </Button>
