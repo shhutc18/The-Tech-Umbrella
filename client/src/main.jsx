9003765271b0fd83549c22f904d870bce37525be
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-17 19:05:05 -0700

    Added profile route to router and navbar

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Login from './components/Login.jsx'
 import Register from './components/Register.jsx'
 import Home from './components/Homepage.jsx'
+import Profile from './components/Profile.jsx'
 
 const router = createBrowserRouter([
   { 
@@ -14,6 +15,7 @@ const router = createBrowserRouter([
       { path: '/login', element: <Login /> },
       { path: '/register', element: <Register /> },
       { path: '/home', element: <Home />},
+      { path: '/profile', element: <Profile />},
     ],
     errorElement: <h1>404 Not Found</h1>
    },
